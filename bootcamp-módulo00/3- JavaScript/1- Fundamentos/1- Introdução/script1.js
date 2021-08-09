207bf183ef3ef119629281ba3a8e565dcf7cbf0d
Anne Karoline Fortunato do Carmo <annekarolinefc@gmail.com>
2021-08-09 16:05:53 -0300

    Revisão de JS - Fundamentos

---
+++ bootcamp-módulo00/3- JavaScript/1- Fundamentos/1- Introdução/script1.js
@@ -0,0 +1,16 @@
+var mensagem = 'Olá';
+mensagem = 'Olá Danilo';
+
+alert(mensagem); //exemplo de pop-up
+console.log(mensagem); // comentário
+
+// comentário de uma linha
+// comentário de uma linha
+
+/*
+
+comentário
+
+console.log(mensagem);
+
+*/
---
+++ bootcamp-módulo00/3- JavaScript/1- Fundamentos/2- Valores e Tipos/primitivos.js
@@ -0,0 +1,10 @@
+// Tipos primitivos
+var vNumber = 5.78;
+var vNumber2 = 6;
+var vString = 'abacaxi';
+var vBoolean = true;
+
+console.log(typeof vBoolean)
+console.log(typeof cString)
+console.log(typeof vNumber)
+console.log(typeof vNumber2)
